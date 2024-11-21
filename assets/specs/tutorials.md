0d75420b3489d9bada7ca4aa8b3ecee03e1a5dd6
RedFox3DMaker <redfox.3dmaker@gmail.com>
2024-11-21 22:07:15 +0100

    add brick picking and changed mouse cursor during play

--- assets/specs/tutorials.md
+++ assets/specs/tutorials.md
@@ -1,6 +1,8 @@
 # Links
-[KidsCanCode](https://kidscancode.org/godot_recipes/4.x/)
-[GDQuest](https://www.gdquest.com/)
-[Brackeys](https://www.youtube.com/c/brackeys/videos)
-[Godot Tutorial TileMapLayer](https://docs.godotengine.org/en/stable/tutorials/2d/using_tilemaps.html)
-[Godot Tutorial TileSet](https://docs.godotengine.org/en/stable/tutorials/2d/using_tilesets.html#doc-using-tilesets)
+- [KidsCanCode](https://kidscancode.org/godot_recipes/4.x/)
+- [GDQuest](https://www.gdquest.com/)
+- [Brackeys](https://www.youtube.com/c/brackeys/videos)
+- [Godot Tutorial TileMapLayer](https://docs.godotengine.org/en/stable/tutorials/2d/using_tilemaps.html)
+- [Godot Tutorial TileSet](https://docs.godotengine.org/en/stable/tutorials/2d/using_tilesets.html#doc-using-tilesets)
+- [Godot Tutorial Mouse cursor](https://docs.godotengine.org/en/stable/tutorials/inputs/custom_mouse_cursor.html#using-project-settings)
+- [Godot Tutorial Mouse drag](https://docs.godotengine.org/en/stable/tutorials/inputs/input_examples.html#mouse-events)
--- scripts/brick.gd
+++ scripts/brick.gd
@@ -1,11 +1,41 @@
 extends RigidBody2D
 
+enum BrickVariantType { TYPE_1, TYPE_2, TYPE_3 }
+@export var variant: BrickVariantType;
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
+class BrickVariant:
+	var region: Vector2
+	var size: Vector2
+	
+	func init(region: Vector2, size: Vector2) -> BrickVariant:
+		self.region = region
+		self.size = size
+		return self
 
+var variant_dict = {
+	BrickVariantType.TYPE_1: BrickVariant.new().init(Vector2(9, 13), Vector2(47,46)),
+	BrickVariantType.TYPE_2: BrickVariant.new().init(Vector2(68,13), Vector2(47,46)),
+	BrickVariantType.TYPE_3: BrickVariant.new().init(Vector2(9,61), Vector2(139,40)),
+}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var dragging = false
+
+func _ready():
+	var brick_variant = variant_dict[variant]
+	$Sprite2D.region_rect = Rect2(brick_variant.region, brick_variant.size)
+	$CollisionShape2D.shape.size = brick_variant.size
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		var sprite_size = $Sprite2D.region_rect.size
+		var relative_position = abs(event.position - $Sprite2D.global_position)
+		if relative_position.x < sprite_size.x and relative_position.y < sprite_size.y:
+			# start dragging if the click is on the sprite
+			if not dragging and event.pressed:
+				dragging = true
+			# stop dragging if the button is released
+		if dragging and not event.pressed:
+			dragging = false
+			
+	if event is InputEventMouseMotion and dragging:
+		$Sprite2D.global_position = event.position
