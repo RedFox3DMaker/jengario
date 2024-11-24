5e4fa77cae6d98c582e01289ad517ff36500eaa1
RedFox3DMaker <redfox.3dmaker@gmail.com>
2024-11-24 18:56:51 +0100

    first restart condition and end screen showing up

--- scripts/brick.gd
+++ scripts/brick.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 class_name Brick
 
 signal clicked
+signal fallen
 
 enum BrickVariantType { TYPE_1, TYPE_2, TYPE_3 }
 @export var variant: BrickVariantType;
@@ -10,7 +11,7 @@ enum BrickVariantType { TYPE_1, TYPE_2, TYPE_3 }
 class BrickVariant:
 	var region: Vector2
 	var size: Vector2
-	
+
 	func init(i_region: Vector2, i_size: Vector2) -> BrickVariant:
 		self.region = i_region
 		self.size = i_size
@@ -27,8 +28,6 @@ func _ready() -> void:
 	var brick_variant = variant_dict[variant]
 	$Sprite2D.region_rect = Rect2(brick_variant.region, brick_variant.size)
 	$CollisionShape2D.shape.size = brick_variant.size
-	$RayCast2DDownLeft.position.x = -brick_variant.size.x / 2
-	$RayCast2DDownRight.position.x = brick_variant.size.x / 2
 	freeze = false
 
 
@@ -44,37 +43,47 @@ const MAX_APPLIED_FORCE: float = 250.0
 var previous_velocity: Vector2 = Vector2.ZERO
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if held:
-		# normalize the direction of the mouse pointer 
+		# normalize the direction of the mouse pointer
 		# and compute velocity to apply force
-		var mouse_pos = get_global_mouse_position()
 		var motion = get_global_mouse_position() - global_transform.origin
 		var delta = state.step;
 		var velocity = motion / state.step
 		var velocity_norm = 0.01 * velocity.length()
-		print("velocity_norm: ", velocity_norm)
 		if velocity_norm >= MAX_VELOCITY:
 			velocity *= MAX_VELOCITY / velocity_norm
-		
+
 		var applied_force = mass * (velocity - previous_velocity) / delta
 		previous_velocity = velocity
 		var applied_force_norm = applied_force.length()
 		if applied_force_norm >= MAX_APPLIED_FORCE:
 			applied_force *= MAX_APPLIED_FORCE / applied_force_norm
-		print("applied_force: ", applied_force)
 		state.apply_force(applied_force)
+
+
+var previous_velocity_y = 980.0
+const GRAVITY_Y: float = 980.0
+const THETA_FALL: float = deg_to_rad(5)
+func _physics_process(delta: float) -> void:
+	if not held and is_in_group("bricks"):
+		var acceleration_y = (linear_velocity.y - previous_velocity_y) / delta
+		previous_velocity_y = linear_velocity.y
+		if acceleration_y >= 0.5 * GRAVITY_Y and rotation >= THETA_FALL:
+			print("fallen")
+			fallen.emit()
 
 
 func pickup() -> void:
 	if held:
 		return
-	#freeze = true
 	sleeping = false
 	held = true
+	previous_velocity_y = 0.0
 
 
 func drop(impulse: Vector2 = Vector2.ZERO) -> void:
 	if held:
-		#freeze = false
 		apply_central_impulse(impulse)
 		previous_velocity = Vector2.ZERO
 		held = false
+		if get_contact_count() == 0:
+			remove_from_group("bricks")
