f53e104781274b2b7f88eb84f7a0fe4fbe0dfb38
RedFox3DMaker <redfox.3dmaker@gmail.com>
2024-11-26 21:24:42 +0100

    first attempt for player idle animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,30 +9,30 @@ enum PlayerVariantType { BOY, ZOMBIE, GIRL, MONSTER }
 func _ready() -> void:
 	var frame: int
 	var label: String
-	var cursor: Resource
+	#var cursor: Resource
 	var color_tone: Color
 	var flip: bool = false
 	match variant:
 		PlayerVariantType.BOY:
 			frame = 0
 			label = "1P"
-			cursor = load("res://assets/ux/cursor_1.png")
+			#cursor = load("res://assets/ux/cursor_1.png")
 			color_tone = Color.CRIMSON
 		PlayerVariantType.ZOMBIE:
 			frame = 6
 			label = "2P"
-			cursor = load("res://assets/ux/cursor_2.png")
+			#cursor = load("res://assets/ux/cursor_2.png")
 			color_tone = Color.LIME_GREEN
 		PlayerVariantType.GIRL:
 			frame = 11
 			label = "3P"
-			cursor = load("res://assets/ux/cursor_2.png")
+			#cursor = load("res://assets/ux/cursor_2.png")
 			color_tone = Color.DARK_ORANGE
 			flip = true
 		PlayerVariantType.MONSTER:
 			frame = 17
 			label = "4P"
-			cursor = load("res://assets/ux/cursor_2.png")
+			#cursor = load("res://assets/ux/cursor_2.png")
 			color_tone = Color.REBECCA_PURPLE
 			flip = true
 		
@@ -43,5 +43,5 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
