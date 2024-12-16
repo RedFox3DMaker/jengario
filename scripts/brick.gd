62846ad76856446fb5807bd844c6862f047464e6
RedFox3DMaker <redfox.3dmaker@gmail.com>
2024-12-16 21:06:30 +0100

    [Brick] add explosion animation

--- scripts/brick.gd
+++ scripts/brick.gd
@@ -7,6 +7,10 @@ extends RigidBody2D
 signal clicked
 signal fallen
 
+@onready var explosion_animation: AnimatedSprite2D = $ExplosionAnimation
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
+
 enum BrickVariantType { SQUARE_DOT, SQUARE, RECTANGLE }
 @export var variant: BrickVariantType
 
@@ -28,8 +32,12 @@ var variant_dict = {
 func _ready() -> void:
 	freeze = true
 	var brick_variant = variant_dict[variant]
-	$Sprite2D.region_rect = Rect2(brick_variant.region, brick_variant.size)
-	$CollisionShape2D.shape.size = brick_variant.size
+	sprite.region_rect = Rect2(brick_variant.region, brick_variant.size)
+	collision_shape.shape.size = brick_variant.size
+	if variant == BrickVariantType.RECTANGLE:
+		explosion_animation.animation = &"rectangle_explosion"
+	else:
+		explosion_animation.animation = &"square_explosion"
 	freeze = false
 
 
@@ -77,6 +85,12 @@ func _physics_process(delta: float) -> void:
 		if acceleration_y >= 0.5 * GRAVITY_Y and rotation >= THETA_FALL:
 			print("fallen")
 			fallen.emit()
+			
+	if not is_in_group("bricks") and get_contact_count() == 0:
+		explosion_animation.visible = true
+		sprite.visible = false
+		explosion_animation.play()
+			
 
 func pickup() -> void:
 	if held:
@@ -94,3 +108,7 @@ func drop(impulse: Vector2 = Vector2.ZERO) -> void:
 			remove_from_group("bricks")
 	else:
 		sound_played = false
+
+
+func _on_explosion_animation_animation_finished() -> void:
+	queue_free()
